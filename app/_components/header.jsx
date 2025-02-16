b5266ed90108460e8bdfcbc54b5b23a2144a9aff
Shrajan8071 <shrajangupta1@gmail.com>
2025-02-16 11:52:15 +0530

    Initial commit

---
+++ app/_components/header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+      <div className="flex flex-row items-center justify-center h-11 w-full text-white bg-slate-800">
+        header component
+      </div>
+    );
+  }
+  
